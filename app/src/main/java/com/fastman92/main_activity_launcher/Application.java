5414b4b1c3f2191a4143decc5def74ef4eff39fc
ripboo <dokan10010@gmail.com>
2026-08-30 12:12:03 +0200

    Update Application.java

--- app/src/main/java/com/fastman92/main_activity_launcher/Application.java
+++ app/src/main/java/com/fastman92/main_activity_launcher/Application.java
@@ -1,1 +1,29 @@
-5
+package com.fastman92.main_activity_launcher;
+
+import android.util.Log;
+import android.widget.Toast;
+
+public class Application extends ApplicationOriginal {
+    
+    @Override
+    public void onCreate() {
+        Functions.CodeOnApplicationLaunch(this);
+        try {
+            SettingsLoader.InitializeSettings(this, false);
+        } catch (Exception e) {
+            SettingsLoader.SetErrorMessage(e.toString());
+            ShowErrorMessage(SettingsLoader.errorMessage);
+        }
+        super.onCreate();
+    }
+
+    void HandleException(Exception e) {
+        ShowErrorMessage(e.toString());
+    }
+
+    void ShowErrorMessage(String msg) {
+        Log.e(MainActivity.log_tag, msg);
+        Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
+    }
+}
+
