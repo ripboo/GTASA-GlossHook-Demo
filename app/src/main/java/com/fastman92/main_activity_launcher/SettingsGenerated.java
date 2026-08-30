1ba053c9f6b70761931791a5df5d3c61b5c1e8d0
ripboo <dokan10010@gmail.com>
2026-08-30 12:18:05 +0200

    Add SettingsGenerated class with build metadata

---
+++ app/src/main/java/com/fastman92/main_activity_launcher/SettingsGenerated.java
@@ -0,0 +1,7 @@
+package com.fastman92.main_activity_launcher;
+
+public class SettingsGenerated {
+    public static final String apkModifierCompileDateTime = "Sat, 12 Oct 2024 08:23:34 GMT";
+    public static final long apkModifierCompileTimestamp = 638643182141565772L;
+    public static final String packageName = "com.rockstargames.gtasa";
+}
